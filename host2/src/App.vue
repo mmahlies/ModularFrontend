a21595d5ed933d03517a5940776d53bae61baf65
mmahlies <eng.m.mahlies@gmail.com>
2021-12-06 22:57:55 +0200

    ui

--- host2/src/App.vue
+++ host2/src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/a">About</router-link>|
-      <router-link to="/about">About lib</router-link>
+      <router-link to="/about">About lib</router-link>|
+         <router-link to="/home">Home lib</router-link>
     </div>
     <router-view/>
   </div>
